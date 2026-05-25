eff7b0b3f4e41b61ae2c296d152a836685cc1964
Vishesh <visheshkhandal2007@gmail.com>
2026-05-25 14:26:22 +0530

    Initial commit

---
+++ 01reactapp/src/App.js
@@ -0,0 +1,12 @@
+
+import chai from './chai';
+function App() {
+  return (
+    <>
+      <chai/>
+      <h1>Chai in React App is Ready</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Chai from "./chai";
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h3>Welcome to Vite React App</h3>
+    </>
+    
+  )
+}
+
+export default App
