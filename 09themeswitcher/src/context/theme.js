e0b705155368c2109d0a98c1b08763a4cfcb2568
Vishesh <visheshkhandal2007@gmail.com>
2026-06-05 14:30:25 +0530

    etc

--- 09themeswitcher/src/context/theme.js
+++ 09themeswitcher/src/context/theme.js
@@ -4,11 +4,10 @@ export const ThemeContext = createContext({
     themeMode: 'light',
     darkTheme: () => {},
     lightTheme: () => {}
-});
+});  
 
 export const ThemeProvider = ThemeContext.Provider;
 
 export default function useTheme() {
     return useContext(ThemeContext);
 }
-    
